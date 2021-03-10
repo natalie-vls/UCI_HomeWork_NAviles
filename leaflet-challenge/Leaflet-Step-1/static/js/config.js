74f987b0eb7ce46be3e7eede90ce2d0e9bebfa94
Natalie Aviles <natalieaviles@natalies-mbp.home>
2021-03-10 09:35:31 -0800

    updates

--- leaflet-challenge/Leaflet-Step-1/static/js/config.js
+++ leaflet-challenge/Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoibmF2aWxlcyIsImEiOiJja2x4MXQ5YTYwMGtsMnByczhvajYxbm9lIn0.3iGu3lE22xP4CAdwlTWtTg";
+const API_KEY = "";
--- python-api-challenge/api_keys.py
+++ python-api-challenge/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "25233509c15b1972d6ea3e4be953184a"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyDOuaTgBX6neo4mP0WWIVHIg2XB0OUdQiY"
+g_key = ""
